242e3809bf490a6196d42f708b1f0d4b93b03df7
dionduren <dionduren94@gmail.com>
2023-10-20 14:42:43 +0700

    add api for list report kategori

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,16 @@ Route::middleware(['auth'])->group(function () {
   Route::get('/create-ticket', [TicketController::class, 'create_ticket'])->name('root');
 
   Route::get('/report-test', [ReportController::class, 'index_test'])->name('report_test');
+
   Route::get('/master/kategori', [MasterDataController::class, 'master_kategori']);
   Route::post('/submit-kategori', [MasterDataController::class, 'submit_kategori']);
   Route::post('/edit-kategori', [MasterDataController::class, 'edit_kategori']);
   Route::post('/delete-kategori', [MasterDataController::class, 'delete_kategori']);
+
+  Route::get('/master/sub-kategori', [MasterDataController::class, 'master_sub_kategori']);
+  Route::post('/submit-sub-kategori', [MasterDataController::class, 'submit_sub_kategori']);
+  Route::post('/edit-sub-kategori', [MasterDataController::class, 'edit_sub_kategori']);
+  Route::post('/delete-sub-kategori', [MasterDataController::class, 'delete_sub_kategori']);
 
   Route::get('{any}', [HomeController::class, 'index'])->name('index');
 });
